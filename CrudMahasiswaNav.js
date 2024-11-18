c4056311c48c58026a77945318d06e7b8fd749d2
heloskanaaziza <heloskanaaziza@gmail.com>
2024-11-18 07:31:17 +0700

    update(11)

--- CrudMahasiswaNav.js
+++ CrudMahasiswaNav.js
@@ -3,22 +3,23 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Profil from './App'
-import Mahasiswa from './Mahasiswa';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faUser, faUserGraduate} from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { WebView } from 'react-native-webview';
+import Createdata from './Createdata';
+import DataMahasiswa from './Listdata'
 
 
 function HomeScreen() {
   return (
-      <Profil/>
+      <Createdata/>
   );
 }
 
 function DataMahasiswaScreen() {
   return (
-    <Mahasiswa/>
+    <DataMahasiswa/>
   );
 }
 
